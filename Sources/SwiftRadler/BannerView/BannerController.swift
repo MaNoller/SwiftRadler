65df8dfef71ccd3edced69228de0b78c802944bd
Mathias Roder <m.roder@evomotiv.de>
2022-12-23 08:40:37 +0100

    Improve Banner Implementation

--- Sources/SwiftRadler/BannerView/BannerController.swift
+++ Sources/SwiftRadler/BannerView/BannerController.swift
@@ -3,34 +3,37 @@ import Combine
 
 public class BannerController: ObservableObject {
    
-//   private let cancelBag = CancelBag()
    @Published private(set) var banner: BannerData? = nil
+   @Published private(set) var isRunning = false
    
    private var queue = Queue<BannerData>()
-   private var timer: Timer? = nil
-   private var isRunning: Bool { return banner != nil }
 
-   public init() {}
+   public init() {
+   }
    
    public func show(banner: BannerData) {
       queue.push(banner)
-      run()
+      if (!isRunning) {
+         run()
+      }
    }
    
    public func hide() {
-      timer?.invalidate()
-      timer = nil
       banner = nil
-      run()
+      Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { [weak self] _ in
+         self?.run()
+      }
    }
    
    private func run() {
       guard !isRunning else { return }
       
       banner = queue.pop()
+      isRunning = banner != nil
+      
       if banner != nil {
-         timer = Timer.scheduledTimer(withTimeInterval: 5, repeats: false) {_ in
-            self.hide()
+         Timer.scheduledTimer(withTimeInterval: 5, repeats: false) { [weak self] _ in
+            self?.hide()
          }
       }
    }
