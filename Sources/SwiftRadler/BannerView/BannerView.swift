2111e032f976372a26fdaff885fdadc13e5129a5
Mathias Roder <m.roder@evomotiv.de>
2023-10-24 14:25:13 +0200

    Improve banner views

--- Sources/SwiftRadler/BannerView/BannerView.swift
+++ Sources/SwiftRadler/BannerView/BannerView.swift
@@ -3,7 +3,9 @@ import SwiftUI
 public struct BannerView: View {
    @ObservedObject var controller: BannerController
    
-   @State var hasBanner = false
+   @State var hasBanner = true
+   
+   static var blue: Color { Color(red: 0.188, green: 0.341, blue: 0.851) }
    
    public init(controller: BannerController) {
       self.controller = controller
@@ -11,7 +13,7 @@ public struct BannerView: View {
    
    private func bgColor(for type: BannerType) -> Color {
       switch type {
-      case .info:      return Color.gray
+      case .info:      return Color.blue
       case .success:   return Color.green
       case .error:     return Color.red
       }
@@ -19,10 +21,19 @@ public struct BannerView: View {
    
    private func textColor(for type: BannerType) -> Color {
       switch type {
-      case .info:      return Color.white
+      case .info:      return Color.black
       case .success:   return Color.black
       case .error:     return Color.black
       }
+   }
+   
+   private func iconName(for type: BannerType) -> String {
+      switch type {
+      case .info:      return "info.circle"
+      case .success:   return "checkmark.circle"
+      case .error:     return "exclamationmark.circle"
+      }
+      
    }
    
    public var body: some View {
@@ -50,24 +61,49 @@ public struct BannerView: View {
       .onChange(of: controller.banner) { (banner) in
          withAnimation { hasBanner = banner != nil }
       }
+      .ignoresSafeArea(.all)
    }
    
    func bannerBox(title: String, message: String, type: BannerType) -> some View {
       HStack {
-         VStack(alignment: .leading, spacing: 2) {
-            Text(title)
-               .bold()
-               .foregroundColor(textColor(for: type))
+         VStack(alignment: .leading, spacing: 9) {
+            HStack(alignment: .center) {
+               Image(systemName: iconName(for: type))
+                  .resizable()
+                  .scaledToFit()
+                  .frame(width: 25)
+                  .foregroundColor(.black)
+                  .offset(y: 9)
+               Text(title)
+                  .bold()
+                  .font(.system(size: 20))
+                  .foregroundColor(textColor(for: type))
+                  .padding(.top)
+            }
             Text(message)
                .font(Font.system(size: 15, weight: Font.Weight.light, design: Font.Design.default))
                .foregroundColor(textColor(for: type))
          }
          Spacer()
       }
-      .frame(minHeight: 130)
-      .foregroundColor(Color.white)
+      .frame(minHeight: 160)
+      .foregroundColor(Color.white) 
       .padding(12)
       .background(bgColor(for: type))
       .cornerRadius(8)
    }
+}
+
+struct BannerView_Previews: PreviewProvider {
+  static var previews: some View {
+     BannerView(controller: makeMockBannerController())
+  }
+}
+
+func makeMockBannerController() -> BannerController {
+   let controller = BannerController()
+   let msg = "Some message that isn't to short but also not very useful"
+   let data = BannerData(title: "Dummy Title", message: msg, type: .error)
+   controller.show(banner: data)
+   return controller
 }
