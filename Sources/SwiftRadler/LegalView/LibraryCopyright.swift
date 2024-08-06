c2337366d7b05408cd47a2558c2936e4b0784632
Marcel Noller <marcelnoller@LaptopvonMarcel.fritz.box>
2024-08-06 15:48:47 +0200

    deleted License enum

--- Sources/SwiftRadler/LegalView/LibraryCopyright.swift
+++ Sources/SwiftRadler/LegalView/LibraryCopyright.swift
@@ -4,13 +4,13 @@ public struct LibraryCopyright: Hashable, Identifiable {
    public let id = UUID()
    public let name: String
    public let version: String
-   public let license: License
+   public let license: String
    public let licenseText: String
    public let copyright: String
    public let shortName: String?
    public let url: String
    
-   public init(name: String, shortName: String? = nil, version: String, license: License, licenseText: String, copyright: String, url: String) {
+   public init(name: String, shortName: String? = nil, version: String, license: String, licenseText: String, copyright: String, url: String) {
       self.name = name
       self.shortName = shortName
       self.version = version
